bd994a87baeaf2ae0ed7dc45e1df5178f23e1021
anuragrao04 <raoanu2004@gmail.com>
2026-04-01 11:34:47 +0530

    fix: tree builder env template substitution bug

--- terrat_runner/work_build_tree.py
+++ terrat_runner/work_build_tree.py
@@ -68,7 +68,6 @@ def run(state):
 
         try:
             env = {'TERRATEAM_BASE_REF': state.work_manifest['base_ref']}
-            env.update(state.env)
             (proc, stdout, stderr) = cmd.run_with_output(
                 state,
                 {
